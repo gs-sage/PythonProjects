f15adf14363dff69e754e440e589453199fdc9d7
gs-sage <gauravsaymi95@gmail.com>
2025-12-06 13:05:10 -0600

    changed logic in greatestNumber.py

--- StringsAndConditionals/greatestNumber.py
+++ StringsAndConditionals/greatestNumber.py
@@ -7,7 +7,7 @@ userInput3 = float(input("Enter third number: "))
 
 if(userInput1>userInput2 and userInput1>userInput3):
     print(f"Highest Number; {userInput1}")
-elif(userInput2>userInput1 and userInput2>userInput3):
+elif(userInput2>userInput3):
     print(f"Highest Number: {userInput2}")
 else:
     print(f"Highest Number: {userInput3}")
