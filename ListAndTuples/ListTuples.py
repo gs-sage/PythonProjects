f537199856db6da086f5ab99d4b722d0d865501a
gs-sage <gauravsaymi95@gmail.com>
2025-12-08 07:52:05 -0600

    added List and Tuples directory with details

---
+++ ListAndTuples/ListTuples.py
@@ -0,0 +1,8 @@
+# Practice file for learning List and Tuples
+
+# Lists
+marks = [90,100,80,50,65] 
+print(marks) # prints the list
+print(len(marks)) # prints the length of the list
+print(type(marks)) # prints the type of the data type
+print(marks[0]) # prints index 0
