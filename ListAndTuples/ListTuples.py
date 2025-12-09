e5b5498c797a9af0e945e455254779b21aa223f6
gs-sage <gauravsaymi95@gmail.com>
2025-12-08 21:28:00 -0600

    described a few functions of list

--- ListAndTuples/ListTuples.py
+++ ListAndTuples/ListTuples.py
@@ -19,3 +19,32 @@ print(marks[1:4]) # prints from index 1 to 3
 print(marks[:5]) # prints all since last value is not included
 print(marks[2:]) # prints from the second index to the last
 print(marks[-3:-1]) #prints 80 and 50 since negative index starts from the rightmost index at -1.
+
+# List functions
+""".append(value) method adds the value to the end of the list"""
+list = [2,1,3] # creating a new list
+fruit = ["mango","apple","dragonfruit"] # to see how string list is sorted
+list.append(4) # adding 4
+print(list)
+
+""".sort() methods sorts the values in ascending order"""
+list.sort() # first sort happens
+fruit.sort() # first sort happens
+print(list)
+print(fruit)
+
+""".sort(reverse=True) method reverses the sort"""
+list.sort(reverse=True) # first reverse happens
+fruit.sort(reverse=True) # first reverse happens
+print(list)
+print(fruit)
+
+""".reverse() method reverses the list"""
+list.reverse() # second reverse happens
+fruit.reverse() # second reverse happens
+print(list)
+print(fruit)
+
+""".insert(index, element) inserts element at a certain index"""
+fruit.insert(3, "pineapple") # inserts pineapple at the 3rd index
+print(fruit)
