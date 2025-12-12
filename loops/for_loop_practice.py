b7969083f2a77bb7386447d1415db51ef7bb8577
gs-sage <gauravsaymi95@gmail.com>
2025-12-11 19:51:56 -0600

    Fixed some logic

--- loops/for_loop_practice.py
+++ loops/for_loop_practice.py
@@ -17,4 +17,14 @@ for i in range(1, 11):
 
 
 """Pass Statement is a null statement that does nothing, it is used as a
-placeholder for future code. """
+placeholder for future code. Only used when we don't want nothing there for 
+the moment"""
+
+for i in range(5):
+    # leaving it empty
+    pass
+
+# pass can also be used in if-else statements
+if(i>5):
+    pass
+
