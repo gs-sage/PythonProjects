bb6ce94220925f9c321e65a6e68a82e15c8a0a7c
gs-sage <gauravsaymi95@gmail.com>
2025-12-12 14:37:52 -0600

    Created functions and recursion directory

---
+++ function_and_recursion/function_practice.py
@@ -0,0 +1,34 @@
+# function to print the length of a list
+def len_list(list):
+    return len(list)
+
+# function to print the elements of a list in a single line
+def el_list(list):
+    for el in list:
+        print(el, end=" ")
+
+
+# function to find the factorial of n
+def factorial(num):
+    factorial = 1
+    for i in range(1, num+1):
+        factorial = factorial * i
+    return factorial
+
+# function to convert USD to INR
+def currency_conversion(currency):
+    conversion_rate = 90.60
+    converted_currency = currency * conversion_rate
+    return converted_currency
+
+new_list = [1,2,3,4,5,6,7,8,9]
+
+print(len_list(new_list)) # prints the length of the list
+el_list(new_list) # prints the element in a list
+print()
+print(factorial(10)) # gets the factorial of a number
+print(currency_conversion(1000)) # converts currency
+
+
+
+    
