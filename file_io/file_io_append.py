1ae7c6b8ac1b4cb61b38367643f136971f105023
gs-sage <gauravsaymi95@gmail.com>
2025-12-15 11:27:09 -0600

    Added notes on write, append, file creation by python with write

---
+++ file_io/file_io_append.py
@@ -0,0 +1,9 @@
+# append function in file i/o
+
+"""Adds data to the end of the file"""
+
+f = open("demo.txt","a")
+f.write("Adding this new line. This will be in the same line without a space at the beginning of this line since we don't have the \\n character") # using escape character for \n otherwise it would send the next part to new line
+f.write("\nThis will be in a new line since we have the newline character in the front. Or we can keept it at the end.\n")
+f.write("And the next lines comes to the new next line.")
+f.close()
