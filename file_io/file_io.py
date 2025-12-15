b12e4a241646961586f2b613aaa23101a527dc7f
gs-sage <gauravsaymi95@gmail.com>
2025-12-15 08:59:46 -0600

    Edited notes in file_io.py file

--- file_io/file_io.py
+++ file_io/file_io.py
@@ -24,5 +24,5 @@ w : open for writing, truncating the file first
 x : create a new file and open it for writing
 a : open for writing, appending to the end of the file if it exists
 b : binary mode
-t : text mode
+t : text mode (default mode when opening a file)
 + : open a disk file for updating(reading and writing)"""
