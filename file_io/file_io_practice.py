b09f5a0b138662f7d8595b4fe9f198db7e1cb93b
gs-sage <gauravsaymi95@gmail.com>
2025-12-16 20:47:23 -0600

    to pull

--- file_io/file_io_practice.py
+++ file_io/file_io_practice.py
@@ -16,6 +16,21 @@ with open("practice.txt","r+") as f:
     data = f.read().replace("Java","Python")
     print(data)
 
+"""IMPORTANT: In the above number 2, we just replaced the values but didn't write it to the file.
+If we want to write it to the file, we would have done something like:
+
+with open("practice.txt","r") as f:
+    data = f.read()
+
+new_data = data.replace("Java","Python") # here we are storing the new values to a new variable
+print(new_data)
+
+Then we would open the file as write now and then just add the new_data variable
+
+with open("practice.txt","w") as f:
+    f.write(new_data) # this will overwrite the file with new data
+    """
+
 # Practice 3: Search for a string in the file you created.
 # opening the file in read mode since we are just searching for a string
 with open("practice.txt","r") as f:
@@ -24,3 +39,10 @@ with open("practice.txt","r") as f:
         print(f"It exists at index {data.find("learning")}.")
     else:
         print("No it doesn't.")
+
+# Practice 4: write a function to find in which line of the file does the word "learning" occur first. Print -1 if word not found
+with open("practice.txt","r") as f:
+   for data in f:
+       line = f.readline()
+       print(line)
+
