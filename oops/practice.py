bb254a3fdcb8ac39157251a232c0cbeedddc5d1f
gs-sage <gauravsaymi95@gmail.com>
2025-12-18 22:27:30 -0600

    Fixed naming errors

--- oops/practice.py
+++ oops/practice.py
@@ -6,12 +6,12 @@ class Account:
     balance = 100
     account_number = 12345
 
-    def debit(self, amount):
+    def credit(self, amount):
         self.balance = self.balance + amount
         print(f"Added ${amount} to account {self.account_number}")
-        print(f"Account {self.account_number} has balance ${self.balance}")
+        print(f"Account {self.account_number} has new balance of ${self.balance}")
 
-    def credit(self, amount):
+    def debit(self, amount):
         self.balance = self.balance - amount
         print(f"Used ${amount} from account {self.account_number}")
         print(f"Account {self.account_number} has new balance of ${self.balance}")
