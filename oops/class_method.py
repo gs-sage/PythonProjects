f2445b45ad064d9f732950c25c62434356d7b786
gs-sage <gauravsaymi95@gmail.com>
2025-12-26 08:33:55 -0600

    Commiting to merge into main

--- oops/class_method.py
+++ oops/class_method.py
@@ -1,5 +1,5 @@
 # Class Method
 """A class method is bound to the class and receives the class as an implicit first argument.
-Note - static method can't access or modify class state and generally for utility.
+Note - static method can't access or modify class state and generally used for utility.
 
 """
