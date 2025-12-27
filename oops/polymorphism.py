612f31193991963395600533e10fba4cb49a271d
gs-sage <gauravsaymi95@gmail.com>
2025-12-26 20:35:36 -0600

    Added dunder functions in polymorphism notes

--- oops/polymorphism.py
+++ oops/polymorphism.py
@@ -22,12 +22,21 @@ class Complex:
     def show_number(self):
         print(f"{self.real}i + {self.imaginary}j")
 
-    def add_complex_number(self, second_object): # self is the object itself and we add another object as variable to add their values
+    def add(self, second_object): # self is the object itself and we add another object as variable to add their values
+        new_real = self.real + second_object.real
+        new_imaginary = self.imaginary + second_object.imaginary
+        return Complex(new_real, new_imaginary)
+    
+    # creating an add function with dunder logic
+    def __add__(self, second_object): # self is the object itself and we add another object as variable to add their values
         new_real = self.real + second_object.real
         new_imaginary = self.imaginary + second_object.imaginary
         return Complex(new_real, new_imaginary)
 
-
+    def __sub__(self, second_object): # self is the object itself and we add another object as variable to subtract their values
+        new_real = self.real - second_object.real
+        new_imaginary = self.imaginary - second_object.imaginary
+        return Complex(new_real, new_imaginary)
 
 num1 = Complex(1,3)
 num1.show_number()
@@ -35,5 +44,10 @@ num1.show_number()
 num2 = Complex(4,5)
 num2.show_number()
 
-add_complex = num1.add_complex_number(num2) # here num1  is self since that is the object calling the add complex number function and we provide num2 as the second object
+add_complex = num1.add(num2) # here num1  is self since that is the object calling the add complex number function and we provide num2 as the second object
 add_complex.show_number()
+
+# after defining a dunder function I can just use the add symbol and add 2 complex numbers
+
+num3 = num1 + num2
+num3.show_number() # this will show the same output as above
