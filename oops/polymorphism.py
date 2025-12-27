0ce3d2838ce35f71efd241ab60c5f76ae1d7213b
gs-sage <gauravsaymi95@gmail.com>
2025-12-26 20:00:46 -0600

    Added notes on polymorphism

---
+++ oops/polymorphism.py
@@ -0,0 +1,12 @@
+"""Polymorphism
+When the same operator is allowed to have different meaning according to the context.
+Good example in python for polymorphism is the operator overloading"""
+
+print(1+2) # prints 3
+print("Name" + " your college") # concatenates two strings
+print([1,2,3] + [4,5,6]) # merges list
+
+"""The above examples are called implicit overloading since the classes for them have
+already been defined in python."""
+
+
