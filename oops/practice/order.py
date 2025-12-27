83206c9a651b6921190f2f280eb400f31da31bf9
gs-sage <gauravsaymi95@gmail.com>
2025-12-26 21:11:43 -0600

    Added logic in order.py

--- oops/practice/order.py
+++ oops/practice/order.py
@@ -9,12 +9,12 @@ class Order:
         first_obj_price = self.price
         second_obj_price = second_obj.price
         if(first_obj_price > second_obj_price):
-            return f"{self.item}({self.price}) is greater than {second_obj.item}({second_obj.price})"
+            return f"{self.item}(${self.price}) is greater than {second_obj.item}(${second_obj.price})"
         else:
-            return f"{second_obj.item}({second_obj.price}) is greater than {self.item}({self.price})"
+            return f"{second_obj.item}(${second_obj.price}) is greater than {self.item}(${self.price})"
         
-ord1 = Order("Pizza", "$15")
-ord2 = Order("Sub", "$11")
+ord1 = Order("Pizza", 15)
+ord2 = Order("Sub", 11)
 
 check_price = ord1 > ord2
 print(check_price)
