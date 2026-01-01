ead6018e9fbcf5a77084d11730de5ba9cde4b30c
gs-sage <gauravsaymi95@gmail.com>
2026-01-01 17:01:33 -0600

    Added logic for url links in html files using name of url in urls.py file, implemented csrf tokens in forms

--- django_projects/django_first_project/tasks/urls.py
+++ django_projects/django_first_project/tasks/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+# this lets django know that this is for the tasks app
+app_name = "tasks"
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("add", views.add, name="add")
