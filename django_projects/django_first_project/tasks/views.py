b940a5f8107843491938838540e7d86e863e8595
gs-sage <gauravsaymi95@gmail.com>
2026-01-01 20:30:30 -0600

    Added server side validation to our forms

--- django_projects/django_first_project/tasks/views.py
+++ django_projects/django_first_project/tasks/views.py
@@ -14,6 +14,19 @@ def index(request):
     })
 
 def add(request):
+    if request.method == "POST":
+        form = new_task_form(request.POST) # if this is left empty then it just creates a blank form
+        # when we use request.POST to fill this class object then it takes the information that the user inputted in the form
+        # and then assigns that value to this variable.
+
+        if form.is_valid(): # checking if the form is valid
+            task = form.cleaned_data["task"] # we are cleaning the data and then only taking the task from the cleaned data
+            tasks.append(task) # now we take that cleaned data task and append it to our tasks list created before
+            
+        else: # if the data is not valid, then we return the same form back to the user
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
     return render(request, "tasks/add.html", {
         "form": new_task_form()
     })
